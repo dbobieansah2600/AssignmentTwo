6fd3e8c33c333bb1402fba2a9f03a33578d1fcd8
Bobie <dbobieansah2600@conestogac.on.ca>
2025-02-15 23:16:57 -0500

    ScaleneTriangle Test

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -6,18 +6,46 @@ namespace UnitTestProject
     [TestFixture]
     public class TriangleTests
     {
-        // Equilateral Triangle Test
+        
+        // Scalene Triangle Tests
         [Test]
-        public void AnalyzeTriangle_ValidEquilateral_ReturnsEquilateral()
+        public void AnalyzeTriangle_ScaleneTriangle1_ReturnsScalene()
         {
-            // Arrange
-            int side1 = 3, side2 = 3, side3 = 3;
-
-            // Act
+            int side1 = 3, side2 = 4, side3 = 5;
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.AreEqual("Scalene triangle", result);
+        }
 
-            // Assert
-            Assert.AreEqual("Equilateral triangle", result);
+        [Test]
+        public void AnalyzeTriangle_ScaleneTriangle2_ReturnsScalene()
+        {
+            int side1 = 6, side2 = 8, side3 = 10;
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.AreEqual("Scalene triangle", result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_ScaleneTriangle3_ReturnsScalene()
+        {
+            int side1 = 7, side2 = 9, side3 = 11;
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.AreEqual("Scalene triangle", result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_ScaleneTriangle4_ReturnsScalene()
+        {
+            int side1 = 12, side2 = 15, side3 = 17;
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.AreEqual("Scalene triangle", result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_ScaleneTriangle5_ReturnsScalene()
+        {
+            int side1 = 9, side2 = 14, side3 = 17;
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.AreEqual("Scalene triangle", result);
         }
 
         

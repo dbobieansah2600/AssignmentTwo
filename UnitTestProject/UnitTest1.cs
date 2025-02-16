50a48788fbd7db3c4e45a7443e50423aa0589e4f
Bobie <dbobieansah2600@conestogac.on.ca>
2025-02-15 23:08:50 -0500

    IsoscelesTriangle commit

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -6,18 +6,30 @@ namespace UnitTestProject
     [TestFixture]
     public class TriangleTests
     {
-        // Equilateral Triangle Test
+       
+        // Isosceles Triangle Tests
         [Test]
-        public void AnalyzeTriangle_ValidEquilateral_ReturnsEquilateral()
+        public void AnalyzeTriangle_IsoscelesTriangle1_ReturnsIsosceles()
         {
-            // Arrange
-            int side1 = 3, side2 = 3, side3 = 3;
-
-            // Act
+            int side1 = 5, side2 = 5, side3 = 3;
             string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.AreEqual("Isosceles triangle", result);
+        }
 
-            // Assert
-            Assert.AreEqual("Equilateral triangle", result);
+        [Test]
+        public void AnalyzeTriangle_IsoscelesTriangle2_ReturnsIsosceles()
+        {
+            int side1 = 4, side2 = 4, side3 = 6;
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.AreEqual("Isosceles triangle", result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_IsoscelesTriangle3_ReturnsIsosceles()
+        {
+            int side1 = 7, side2 = 7, side3 = 5;
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+            Assert.AreEqual("Isosceles triangle", result);
         }
 
         
